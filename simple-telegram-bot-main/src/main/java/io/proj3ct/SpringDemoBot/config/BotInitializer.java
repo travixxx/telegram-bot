0b23c283fe0336d573115b212a455bfc8f3ee0d1
Aliaksandr <83635697+travixxx@users.noreply.github.com>
2024-06-26 08:42:53 +0300

    Add files via upload

---
+++ simple-telegram-bot-main/src/main/java/io/proj3ct/SpringDemoBot/config/BotInitializer.java
@@ -0,0 +1,29 @@
+package io.proj3ct.SpringDemoBot.config;
+
+import io.proj3ct.SpringDemoBot.service.TelegramBot;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BotInitializer {
+
+    private final TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
---
+++ simple-telegram-bot-main/src/main/java/io/proj3ct/SpringDemoBot/service/TelegramBot.java
@@ -0,0 +1,71 @@
+package io.proj3ct.SpringDemoBot.service;
+
+
+import io.proj3ct.SpringDemoBot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.webapp.WebAppInfo;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class TelegramBot extends TelegramLongPollingBot {
+    final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            long chatId = update.getMessage().getChatId();
+            sendWelcomeMessage(chatId);
+        }
+    }
+
+    private void sendWelcomeMessage(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(
+            "Привет, я очень рада, что ты решил поиграть в игру вместе со мной! Жми кнопку \"Сайт с той самой игрой\" и вперед зарабатывать!");
+
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+        InlineKeyboardButton siteButton = new InlineKeyboardButton();
+        siteButton.setText("Сайт с той самой игрой");
+        siteButton.setWebApp(new WebAppInfo("https://gomines.ru/bestigr"));
+
+        rowInline.add(siteButton);
+        rowsInline.add(rowInline);
+
+        keyboardMarkup.setKeyboard(rowsInline);
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
